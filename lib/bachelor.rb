5b2caad817c9da72c8946bb54ab4bafe8055a755
Patricia Carter <patricia4194@gmail.com>
2018-08-17 14:32:09 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,75 @@
+require 'pry'
+
+=begin data = { 
+  "season 30": [
+    {
+      "name":      "Beth Smalls",
+      "age":       "26",
+      "hometown":  "Great Falls, Virginia",
+      "occupation":"Nanny/Freelance Journalist",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Becca Tilley",
+      "age":        "27",
+      "hometown":   "Shreveport, Louisiana",
+      "occupation": "Chiropractic Assistant",
+      "status":     "Eliminated Week 8"
+    }
+  ],
+  "season 29": [
+    {
+      "name":      "Ashley Yeats",
+      "age":       "24",
+      "hometown":  "Denver, Colorado",
+      "occupation":"Dental Assitant",
+      "status":    "Winner"
+    },
+    {
+      "name":       "Sam Grover",
+      "age":        "29",
+      "hometown":   "New York, New York",
+      "occupation": "Entertainer",
+      "status":     "Eliminated Week 6"
+    }
+  ]
+=end }
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season][0].each do |contestant_value, contestant_data|
+    if data[season][0]["status"] == "Winner"
+      name = data[season][0]["name"].split(' ')
+      return name.first
+    end 
+  end 
 end
 
+=begin def get_contestant_name(data, occupation)
+  data.each do |season, wrapper|
+    #binding.pry
+    wrapper.each do |contestant_value|
+        #binding.pry
+        if contestant_value[:occupation] == occupation
+          puts contestant_value[:name]
+          return contestant_value[:name]
+      end
+    end
+  end
+=end end
+
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, wrapper|
+    data[season].each do |contestant_value|
+      puts contestant_value["occupation"]
+        if contestant_value[:occupation] == occupation
+          puts contestant_value[:name]
+          return contestant_value[:name]
+      end
+    end
+  end
 end
+
+#get_contestant_name(data, 'Entertainer')
 
 def count_contestants_by_hometown(data, hometown)
   # code here

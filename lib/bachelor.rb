082d73e0ef38302131b4cc36b79ca54c669ab288
Patricia Carter <patricia4194@gmail.com>
2018-08-17 15:03:44 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -82,6 +82,7 @@ def get_average_age_for_season(data, season)
   binding.pry
   data[season].each do |contestant_value|
      total_age += contestant_value["age"].to_f
+     binding.pry
   end 
   return (total_age / total_contestants)
 end

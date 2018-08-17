ed608b5ca7344e2166cb1c7050d1722d56b44264
Patricia Carter <patricia4194@gmail.com>
2018-08-17 14:52:15 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,7 +55,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  hometown_counter = 0
+  data.each do |season, wrapper|
+    wrapper.each do |contestant_value|
+        if contestant_value["hometown"] == hometown
+          hometown_counter += 1
+      end
+    end
+  end
+  return hometown_counter
 end
 
 def get_occupation(data, hometown)
